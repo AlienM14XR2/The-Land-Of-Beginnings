e17dd35945cec094d3e718e6ef99f29f52cc4a8c
alienm14xr2 <alienm14xr2@gmail.com>
2023-05-30 15:41:49 +0900

    cli.c 標準入出力、その出力表現の学習。 - 無限ループによる監視機能、その足掛かりを用意した。 - 独自コマンドを関数として扱う、そう考えている、今は：）

--- src/cli.c
+++ src/cli.c
@@ -14,9 +14,23 @@ void println(const char* message){
 void ptr_d_debug(const char* message, int* debug) {
     printf("%s\tvalue=%d\taddr=%p\n",message,*debug,debug);
 }
-void ptr_f_debug(const char* message, double* debug) {
-    printf("%s\tvalue=%f\taddr=%p\n",message,*debug,debug);
+void ptr_lf_debug(const char* message, double* debug) {
+    printf("%s\tvalue=%lf\taddr=%p\n",message,*debug,debug);
 }
+void ptr_str_debug(const char* message, char* debug) {
+    printf("%s\tvalue=%s\taddr=%p\n",message,debug,debug);
+}
+
+int monitoring() {
+    char cmd[256]={'\0'};
+    printf("ORx2> ");    
+    scanf("%s", cmd);
+    ptr_str_debug("cmd is ",cmd);
+}
+int cmd_exit() {
+    return 0;
+}
+
 
 
 int main(void) {
@@ -25,12 +39,17 @@ int main(void) {
         int x = 3;
         double pai = 3.141592;
         ptr_d_debug("x is ",&x);
-        ptr_f_debug("pai is ",&pai);
+        ptr_lf_debug("pai is ",&pai);
         int* px = &x;
         double* pp = &pai;
+        char str[256] ="I'm Jack.\n";
         ptr_d_debug("px is ",px);
-        ptr_f_debug("pp is ",pp);
+        ptr_lf_debug("pp is ",pp);
+        ptr_str_debug("str is ",str);
     }
+    // CLI の無限ループ
+    monitoring();
+    // Command によりループを脱出できる
     println("=============== Command Line Interface END");
     return 0;
 }
