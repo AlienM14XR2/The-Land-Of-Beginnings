b6bc8f07a4a8b8a1ea6de69870afbac93e0ab943
alienm14xr2 <alienm14xr2@gmail.com>
2023-06-06 13:28:02 +0900

    cli.c 標準入出力、その入力、出力表現の学習。 - ここで、自分の二次元配列と多重ポインタの理解が全くないことを知る、この二つを学習してみる。 - two_dimensional_array.c これで疑問を払拭する、少し理解が進んだ。 - ORx2はここでペンディング、半角スペースが無駄に入力された時の動確を次回リスタート時はやること：）

---
+++ src/two_dimensional_array.c
@@ -0,0 +1,59 @@
+/**
+    二次元配列と二重ポインタの学習
+    
+    これは、やったと思うが完全に記憶にない、やったことないのかもしれない：）
+    二次元配列と二重ポインタは別々で扱ったことがあったが、併用したことがないのかな。
+    だから、ここではっきりさせる、自分の分からない部分を、そして改善する。
+  
+    この考え方は面白い：）  
+  https://qiita.com/chihayafuru/items/6647bed415121c62c8dd
+  二次元配列もポインタで一次元配列のように扱ってしまう。
+    
+*/
+#include "stdio.h"
+#include "stdlib.h"
+#define ROWS 2
+#define COLS 4
+
+void println(const char* message) {
+    printf("%s\n",message);
+}
+void ptr_d_debug(const char* message, int* debug) {
+    printf("%s\tvalue is %d\t addr is %p\n",message,*debug,debug);
+}
+void ptr_str_debug(const char* message, char* debug) {
+    printf("%s\tvalue is %s\t addr is %p\n",message,debug,debug);
+}
+int test_two_dimensional_array() {
+    println("-------------------------------------- test_two_dimensional_array");
+    int square[ROWS][COLS] = {
+        {1,2,3,4},        
+        {11,12,13,14}        
+    };
+    int (*top_of_rows)[COLS];
+    int *top_of_cols;
+    top_of_rows = square;
+    top_of_cols = top_of_rows[1];
+    
+    ptr_d_debug("square[1][1] is ",&top_of_cols[1]);
+    // これは目から鱗ですよ：）
+    // 次のものは少し冗長に感じてしまうかもしれないが、ポインタ配列を実践してみる。
+    // これも参考サンプルのまんま：）
+    return 0;
+}
+
+int main(void) {
+    println("START TWO DIMENSIONAL ARRAY ===============");
+    if(1) {
+        int x = 3;
+        int* px = &x; 
+        ptr_d_debug("px ... ",px);
+        char moji[] = {"ABCDEFG."};
+        ptr_str_debug("moji ... ",moji);
+    }
+    if(2) {
+        test_two_dimensional_array();
+    }
+    println("=============== TWO DIMENSIONAL ARRAY END");
+    return 0;
+} 
