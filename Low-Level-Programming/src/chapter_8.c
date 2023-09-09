2b7212e5fb723bf530e0be0fb920bf2a0c3ff93b
alienm14xr2 <alienm14xr2@gmail.com>
2023-09-09 14:48:58 +0900

    LOW-LEVEL PROGRAMMING。 - 気になるものを随時試す。 - 最初の約数を求める関数。

--- Low-Level-Programming/src/chapter_8.c
+++ Low-Level-Programming/src/chapter_8.c
@@ -16,10 +16,34 @@ gcc -o ../bin/main -std=c11 -pedantic-errors -Wall -Werror chapter_8.c
 */
 #include "stdio.h"
 
+/**
+    最初の約数を見つける。
+    引数 n をを受取、1 より大きく n 以下の整数から、n を割り切れる最小の数 r を探す。
+    因みにもし r = n ならそれは当然素数である。
+*/
+int first_divisor(int n) {
+    int i;
+    if(n == 1) return 1;
+    for(i=2 ; i<=n; i++) {
+        if(n%i==0) return i;
+    }
+    return 0;
+}
 int main(void) {
-    for(int i=0; i<=10; i++) {  // このFor 文内での変数宣言は note: use option ‘-std=c99’, ‘-std=gnu99’, ‘-std=c11’ or ‘-std=gnu11’ to compile your code ということだ。
-        if(i%2) {continue;}
-        printf("%d は偶数です。 \n",i);
+    // For 文
+    if(8.33) {
+        puts("For 文 ---------");  // 改行コードもいれてくれるのか：）
+        for(int i=0; i<=10; i++) {  // このFor 文内での変数宣言は note: use option ‘-std=c99’, ‘-std=gnu99’, ‘-std=c11’ or ‘-std=gnu11’ to compile your code ということだ。
+            if(i%2) {continue;}
+            printf("%d は偶数です。 \n",i);
+        }
+    }
+    // 例：約数
+    if(8.36) {
+        puts("約数 ---------");
+        for(int i=1; i<11; i++) {
+            printf("n is %d ... first divisor is %d\n",i,first_divisor(i));
+        }
     }
     return 0;
 }
