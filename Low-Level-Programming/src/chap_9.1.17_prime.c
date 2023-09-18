e4bedd10ce6276f08d59979eadd16e9ba280aba3
alienm14xr2 <alienm14xr2@gmail.com>
2023-09-18 16:40:12 +0900

    LOW-LEVEL PROGRAMMING。 - 第９章   型システム。 - 課題：素数の判定。 - ptr_is_prime 仮引数の値もアドレスも変更不可能なものを定義した、その実装とテスト。

--- Low-Level-Programming/src/chap_9.1.17_prime.c
+++ Low-Level-Programming/src/chap_9.1.17_prime.c
@@ -9,6 +9,11 @@
   - main 関数は、unsigned long 型の数を 1 つ読んで、それを引数として is_prime 関数を呼び出す。そして結果によって yes か no を出力する。
   
     まずは  man scanf を読もう。 scanf 関数を書く時に書式指定子は %lu を使うこと。 unsigned long を  is_prime は受け取るため。
+  
+  e.g. compile
+  ```
+  gcc -std=c11 -pedantic-errors -Wall -Werror chap_9.1.17_prime.c -o ../bin/main
+  ```
     
   @author jack    
 */
@@ -36,6 +41,7 @@ int is_prime(const unsigned long n) {
     }
 }
 int test_is_prime() {
+    puts("test_is_prime ---------");
     unsigned long i = 0;
     int sum = 0;
     for(i=0; i<=100; i++) {
@@ -50,10 +56,48 @@ int test_is_prime() {
     printf("sum is %d\n",sum);
     return 0;
 }
+int ptr_is_prime(const unsigned long* const n) {
+    if(*n == 0 || *n == 1) {
+        return 0;    // 素数ではないから。
+    }
+    unsigned long i = 0;
+    int count = 0;
+    for(i=2; i<=*n; i++) {
+        if(!(*n%i)) {    // 余りなく割り切れた場合はカウンタをインクリメントする。
+            count++;
+        }
+        if(count >= 2) {
+            break;
+        }
+    }
+    if( count == 1 ) {
+        return 1;
+    } else {
+        return 0;
+    }
+
+}
+int test_ptr_is_prime() {
+    puts("test_ptr_is_prime ---------");
+    unsigned long i = 0;
+    int sum = 0;
+    for(i=0; i<=100; i++) {
+        int ret = ptr_is_prime(&i);
+        if(ret) {
+            printf("%lu is prime.\n",i);
+            sum++;
+        } else {
+            printf("%lu is not prime.\n",i);
+        }
+    }
+    printf("sum is %d\n",sum);
+    return 0;
+}
 int main(void) {
     puts("9.1.17 課題：素数の判定 =========");
     test_is_prime();
-    
+    test_ptr_is_prime();
+       
     unsigned long x = 0;
     printf("input number ... and enter.\n");
     scanf("%lu",&x);
