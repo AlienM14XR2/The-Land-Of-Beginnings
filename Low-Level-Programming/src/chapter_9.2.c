d5a588061542168f575468fc6193454da5bdf0b1
alienm14xr2 <alienm14xr2@gmail.com>
2023-09-18 21:45:41 +0900

    LOW-LEVEL PROGRAMMING。 - 第９章   型システム。 - 9.2.1 構造体（9.2 タグのある型）。 - 構造体のタグについて。

--- Low-Level-Programming/src/chapter_9.2.c
+++ Low-Level-Programming/src/chapter_9.2.c
@@ -14,5 +14,34 @@
 
 int main(void) {
     puts("9.2 タグのある型 （struct, union, enum）=========");
+    if("9.2.1") {
+        puts("9.2.1 構造体 ------");
+        // タグという概念
+        struct pair {
+            int a;
+            int b;
+        };
+        
+        struct pair d;  // pair は単なるタグであり、 struct pair ではじめてコンパイラに正しく認識される。
+        d.a = 0;
+        d.b = 1;
+        printf("d.a is %d\n",d.a);
+        printf("d.b is %d\n",d.b);
+        if("9-40") {
+            puts("9-40 ---");
+            /**
+                                    それぞれ struct type と type という型の２つの変数を、コンパイラが完全に受け入れることを示すデモンストレーションだ。
+            */
+            typedef unsigned int type;
+            struct type {
+                char c;
+            };
+            struct type st;
+            st.c = 'A';
+            type t = 33;
+            printf("st.c is %c ... %d\n",st.c,st.c);
+            printf("t is %d\n",t);
+        }
+    }
     return 0;
 }
