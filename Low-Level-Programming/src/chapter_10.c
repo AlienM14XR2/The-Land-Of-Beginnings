e5b71b7692499e7c17f26def2782aac526a69533
alienm14xr2 <alienm14xr2@gmail.com>
2023-09-23 18:23:08 +0900

    LOW-LEVEL PROGRAMMING。 - 第１０章  コードの構造 - 宣言と定義について。 - 一応ソースファイルを用意した（中身は空）。

---
+++ Low-Level-Programming/src/chapter_10.c
@@ -0,0 +1,21 @@
+/**
+        第１０章
+        コードの構造
+        
+        宣言と定義
+        
+    ```
+    gcc -std=c11 -pedantic-errors -Wall -Werror chapter_10.c -o ../bin/main
+    ```
+    @author jack
+*/
+#include "stdio.h"
+
+int main(void) {
+    // この章はサンプルをとることはあまりないかもしれないが、念の為用意した。
+    // どちらかといえば、実践内容をメモすることになりそう。
+    if("10.1") {
+        puts("10.1 宣言と定義 =========");
+    }
+    return 0;
+}
