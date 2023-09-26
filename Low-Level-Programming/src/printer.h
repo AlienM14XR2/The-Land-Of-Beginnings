0294cd6a45c4c3fe8b22cb6e2a4ba426245231dd
alienm14xr2 <alienm14xr2@gmail.com>
2023-09-27 00:53:16 +0900

    LOW-LEVEL PROGRAMMING - 第１０章  コードの構造 - インクルードガードについて。 - printer.h にその書き方を追加した。

--- Low-Level-Programming/src/printer.h
+++ Low-Level-Programming/src/printer.h
@@ -1,3 +1,12 @@
+/**
+    インクルードガードの例
+    次のようにすることで、実行ファイル作成時に関数が
+    多重定義されることを防ぐことができる。
+*/
+#ifndef _PRINTER_H_     // _PRINTER_H_ が定義されていなかったら 
+#define _PRINTER_H_     // _PRINTER_H_ を定義するし、以下の関数のプロトタイプ宣言が読まれる。
+
 void print_one(void);
 void print_two(void);
 
+#endif                  // ifndef ディレクティブの終わり
