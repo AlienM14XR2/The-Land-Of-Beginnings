6d4b4304ac468bf68740d0ddaf146a8d9702048d
alienm14xr2 <alienm14xr2@gmail.com>
2023-10-01 20:03:53 +0900

    LOW-LEVEL PROGRAMMING - 第１０章  コードの構造 - 動的メモリ割り当てについて。 - chapter_10.5.c にて動的配列の初期化関数を定義した。

--- Low-Level-Programming/src/chapter_10.5.c
+++ Low-Level-Programming/src/chapter_10.5.c
@@ -5,15 +5,27 @@
 #include "stdio.h"
 #include "malloc.h"
 
+int init_array(int* array, size_t size) {
+    size_t i;
+    for(i = 0; i < size; i++) {
+        array[i] = 0;
+    }
+    return 0;
+}
 int main(void) {
     if("10.5.1") {
         puts("動的メモリ割り当て（紹介）");
         int* array;
+        size_t size = 10;
         // malloc は割り当てたメモリの開始アドレスを返す。
         // 引数はバイトサイズであり、その場合は
         // 要素数に要素のサイズを掛けたもの。
-        array = malloc(10 * sizeof(int));
+        array = malloc(size * sizeof(int));
         
+        init_array(array,size);
+        for(size_t i = 0; i < size; i++) {
+            printf("array[%lu] is %d\taddress is %p\n",i,array[i],(void*)&array[i]);
+        }
         // ここでメモリ領域の割り当てを解放する。
         free(array);
     }
