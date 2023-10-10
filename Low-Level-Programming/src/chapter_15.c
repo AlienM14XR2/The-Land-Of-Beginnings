11ea59bcab7b3ce75032b33ac20246d9ef7de605
Jack <alienm14xr2@gmail.com>
2023-10-10 21:40:24 +0900

    第１５章    共有オブジェクトとコードモデル - C の動的ライブラリについて。 - Global Offset Table と Program Linkage Table について。

--- Low-Level-Programming/src/chapter_15.c
+++ Low-Level-Programming/src/chapter_15.c
@@ -44,5 +44,21 @@ int main(void) {
         ldd main
         ```
     */
+    /**
+        15.4 GOT（Global Offest Table） と PLT（Program Linkage Table）
+         
+                  オブジェクトで定義される保証のないシンポルは、静的リンクを行った後の実行ファイルまたは共有オブジェクトファイルで定義されている全部のシンポルを間接参照する。
+        ```
+        objdump -D -Mintel-mnemonic ../bin/dynlib.o
+        objdump -D -Mintel-mnemonic ../bin/main
+        ```
+    */
+    /**
+        15.4.2 外部の関数を呼び出す
+         
+                  一般にプログラマが動的ライブラリを、自分のプログラムで使うためにリンクするときは、しばしばサードパーティ製のライブラリが入る。
+                  それには実際に呼び出す必要のある関数より、ずっと多くの関数が入っている。
+                  そのため、特別なPLT（Program Linkage Table）を介した、もう１段階の間接参照が加わっている。
+    */
     return 0;
 }
