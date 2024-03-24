17f50fd47ef93a3e05c898b00e21357b2d115b40
Jack <alienm14xr2@gmail.com>
2024-03-24 18:02:56 +0900

    C 課題 - void* について。 - 個人的な興味。 - test_memset() 定義。

--- src/void_pointer.c
+++ src/void_pointer.c
@@ -8,11 +8,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 
 
+
+int test_memset(void) {
+  /**
+        汎用ポインタとして「void型ポインタ」を使っている代表的なライブラリ関数
+    void* memset(void* dst, int val, size_t size);
+        
+  */    
+  puts("------ test_memset");
+  char moji[256];
+  void* ret = memset(moji, '\0', 256);   // 内部で、各型のポインタで型キャストしている。
+  strcat(moji, "foo");
+  printf("moji is %s\taddr is %p\n", moji, (void*)moji);
+  printf("void* ret \taddr is %p\n", ret);
+  
+  return 0;
+}
 
 int main(void) {
   puts("START void ポインタについて ===");
+  if(1.00) {
+    int ret = 0;
+    printf("Play and Result ... %d\n", ret = test_memset());
+    assert(ret == 0);
+  }
   puts("===   void ポインタについて   END");
   return 0;
 }
