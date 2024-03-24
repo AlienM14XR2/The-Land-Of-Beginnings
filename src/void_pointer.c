4b998043061ab4c05094a4999b05a8fd64fe245c
Jack <alienm14xr2@gmail.com>
2024-03-24 17:24:33 +0900

    C 課題 - void* について。 - 個人的な興味。 - 作業用ソースファイルを作成。

---
+++ src/void_pointer.c
@@ -0,0 +1,18 @@
+/**
+  課題
+ void ポインタについて
+ 
+ gcc -O3 -DDEBUG -std=c17 -pedantic-errors -Wall -Werror void_pointer.c -o ../bin/main
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+
+int main(void) {
+  puts("START void ポインタについて ===");
+  puts("===   void ポインタについて   END");
+  return 0;
+}
