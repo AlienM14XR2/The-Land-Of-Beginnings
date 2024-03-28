4a13164ae5612dad7e25772dac37baac72bee141
Jack <alienm14xr2@gmail.com>
2024-03-28 22:41:47 +0900

    C 課題 - 文字列の解析。 - 個人的な興味。 - ファイルサイズの取得、メモリ取得、解放の各関数とその動確。

--- src/string_parse.c
+++ src/string_parse.c
@@ -37,13 +37,101 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include <sys/stat.h>
 
+
+
+void debug_int(const char* _message, const int* _debug) {
+  printf("DEBUG: %s\tval: %d\taddr: %p\n", _message, *_debug, (void*)_debug);  
+}
+
+void debug_double(const char* _message, const double* _debug) {
+  printf("DEBUG: %s\tval: %lf\taddr: %p\n", _message, *_debug, (void*)_debug);  
+}
+
+void debug_long(const char* _message, const void* _debug) {
+  size_t* psz = (size_t*)_debug;
+  printf("DEBUG: %s\tval: %lu\taddr: %p\n", _message, *psz, _debug);
+}
+
+void print_error(const char* message) {
+  printf("ERROR: %s\n", message);
+}
+
+
+static const char* FILE_PATH = "/home/jack/tmp/sample.html";
+
+/**
+    ファイルの読み込み。
+    指定サれたファイルをプログラムで利用できるようにする
+    
+    - ファイルサイズの取得
+    - ファイルサイズ + 1 のメモリ取得
+    - ファイルの Read
+  まずは、以上を達成してみる。
+*/
+
+
+size_t getFileSize(const char* fileName) {
+  puts("--- getFileSize");
+  struct stat st;
+  if(stat(fileName, &st) != 0) {
+    return 0ul;
+  }
+  printf("S_ISREG(st.st_mode) is \t%d\n", S_ISREG(st.st_mode));
+  if(S_ISREG(st.st_mode)) {
+    return st.st_size;
+  } else {
+    return 0ul;
+  }
+}
+
+int test_getFileSize() {
+  puts("=== test_getFileSize");
+  size_t size = getFileSize(FILE_PATH);
+  debug_long("size is ", &size);
+  assert(size != 0ul);
+  return EXIT_SUCCESS;
+}
+
+void createBuffer(char* _buffer, size_t _size) {
+  puts("--- createBuffer");
+  debug_long("size is ", &_size);
+  _buffer = (char*)malloc(_size);
+  if(_buffer == NULL) {
+    print_error("malloc faile.");    
+    exit(EXIT_FAILURE);
+  }
+  _buffer = '\0';
+}
+
+void removeBuffer(char* _buffer) {
+  puts("--- removeBuffer");
+  free((void*)_buffer);
+}
+
+int test_Create_Remove_Buf() {
+  puts("=== test_Create_Remove_Buf");
+  char* buf = NULL;
+  size_t size = getFileSize(FILE_PATH);
+  size += 1;
+  createBuffer(buf, size);
+  removeBuffer(buf);  
+  return EXIT_SUCCESS;
+}
 
 
 
 int main(void) {
   puts("START 文字列の解析 ===");
+  if(1.00) {
+    int ret = 0;
+    printf("Play and Result ... %d\n", ret = test_getFileSize());
+    assert(ret == 0);
+    printf("Play and Result ... %d\n", ret = test_Create_Remove_Buf());
+    assert(ret == 0);
+  }
   puts("===   文字列の解析  END");
-  return 0;
+  return EXIT_SUCCESS;
 }
 
