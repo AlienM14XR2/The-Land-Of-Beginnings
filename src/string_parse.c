6d453a43a27002110eb1d80681c0863ce04d3d1b
Jack <alienm14xr2@gmail.com>
2024-03-29 19:47:39 +0900

    C 課題 - 文字列の解析。 - 個人的な興味。 - searchProto 関数とそのテストを実装。

--- src/string_parse.c
+++ src/string_parse.c
@@ -41,6 +41,185 @@
 
 
 
+
+/**
+  Linked List 
+  
+  void* を利用したものを考えてみる。
+    データ構造は  value と next で表現できるはず。
+  add() で追加、remove() で削除する。
+*/
+
+struct tree {
+  void* value;
+  struct tree* next;
+};
+
+typedef void* H_TREE;
+
+/**
+    ツリーハンドラの作成。
+*/
+H_TREE createTree() {
+  puts("--------- createTree");
+  struct tree* pt = (struct tree*)malloc(sizeof(struct tree));
+  pt->value = NULL;
+  pt->next = NULL;
+  return (H_TREE)pt;
+}
+
+/**
+    ツリーハンドラの次の値を返却する。
+*/
+H_TREE hasNext(H_TREE _tree) {
+  return (H_TREE)((struct tree*)_tree)->next;
+}
+
+/**
+    ツリーハンドラのポインタを最後尾に移動する。
+*/
+H_TREE moveLast(H_TREE _tree) {
+  puts("--------- moveLast");
+  struct tree* last = (struct tree*)_tree;
+  while(last != NULL) {
+    if(last->next != NULL) {
+      last = last->next;
+    } else {
+      break;
+    }
+  }
+//  printf("last addr is \t%p\n", (void*)last);
+  return (H_TREE)last;
+}
+
+/**
+    ツリーハンドラの要素数と根の合計数を返却する。
+*/
+size_t countTree(H_TREE _root) {
+  puts("--------- countTree");
+  struct tree* current = (struct tree*)_root;
+  size_t i = 0;
+  while((current = hasNext(current)) != NULL) {
+    i++;
+  }
+  i += 1;     // これは root も含めて返却している。
+  return i;
+}
+
+/**
+    ツリーハンドラの全要素の削除を行う。
+    根も削除する。
+    ※ もし、H_TREE が管理する value が動的にメモリ確保されたもので、H_TREE 以外そのアドレスを知り得ない場合
+    この関数を value 解放前に行うとメモリリークを引き起こす：）
+*/
+void clearTree(H_TREE _root, size_t count) {  // このことは「肝に銘じよ」C 言語では削除する対象のサイズを先に計算しろ。
+  puts("--------- clearTree");
+  H_TREE array[count];    // この配列の宣言のやり方は GCC 以外でもできるのかな？ 添字に変数を利用しているんだよね。
+  struct tree* current = (struct tree*)_root;
+  array[0] = current;
+  size_t i = 1;
+  while((current = hasNext(current)) != NULL) {
+    array[i] = current;
+    i++;
+  }
+  for(i=0; i<count; i++ ) {
+//    puts("------------ D");
+    free(array[i]);
+  }
+}
+
+/**
+    ツリーハンドラに値を追加する。
+*/
+H_TREE pushTree(H_TREE _root, void* value) {
+  puts("--------- pushTree");
+  struct tree* pt = (struct tree*)malloc(sizeof(struct tree));
+  pt->value = value;
+  pt->next = NULL;
+  struct tree* last = (struct tree*)moveLast(_root);
+  if(last == (struct tree*)_root) {
+//      printf("------------ SAME\n");    
+    ((struct tree*)_root)->next = pt;
+  } else {
+    last->next = pt;
+  }
+//  printf("pt addr is \t%p\n", (void*)pt);
+  return (H_TREE)pt;
+}
+
+void* getValue(H_TREE current) {
+  puts("--------- getValue");
+  if(current != NULL) {
+    return ((struct tree*)current)->value;
+  } else {
+    return NULL;
+  }
+}
+
+/**
+    最後尾にある値の取り出しとそのツリーハンドラの削除を行う。
+  Stack と同じ動作、最初にツリーハンドラに登録した値は、最後に取り出される（First-in Last-out）。
+*/
+void* popStack(H_TREE _root) {
+  puts("--------- popStack");
+  // 前方アドレスの付け替え後、削除する。
+  void* value = NULL;
+  struct tree* last     = (struct tree*)moveLast(_root);
+  struct tree* current  = (struct tree*)_root;
+  if(current == last) {
+    // ignore
+    return NULL;
+  } 
+  else
+  if(current->next == last) {
+//    printf("------------ F2 SAME\n");
+    value = last->value;
+    current->next = NULL;
+    free((void*)last);
+  } 
+  else {
+    while((current = (H_TREE)hasNext((H_TREE)current)) != NULL) {
+      if( ((struct tree*)current)->next == last ) {
+//        printf("------------ SAME\n");
+        value = last->value;
+        ((struct tree*)current)->next = NULL;
+        free((void*)last);
+        break;
+      }
+    }
+  }
+  return value;
+}
+
+/**
+    起点の次、仮の先頭の値の取り出しとそのツリーハンドラの削除を行う。
+  queue と同じ動作（First-in First-out）。
+*/
+void* popQueue(H_TREE _root) {
+  puts("--------- popQueue");
+  // 先頭の ツリーハンドラは起点なので無視する
+  // 2 番目が対象であり、3 番目がある場合はそのアドレスを起点の next に代入する
+  void* value = NULL;
+  struct tree* second = ((struct tree*)_root)->next;
+  if(second != NULL) {
+    value                       = second->value;
+    ((struct tree*)_root)->next = second->next;
+    free((void*)second);
+  }
+  return value;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 void debug_int(const char* _message, const int* _debug) {
   printf("DEBUG: %s\tval: %d\taddr: %p\n", _message, *_debug, (void*)_debug);  
 }
@@ -137,16 +316,16 @@ int test_Create_Remove_Buf() {
 void readFile(const char* _filePath, char* _buf) {
   puts("--- readFile");
   FILE*  fp          = NULL;
-  char   tmp[1024+1] = "\0";
+  char   tmp[2048+1] = "\0";
   size_t readSize    = 0;
     
   fp = fopen(_filePath, "r");
   if(fp != NULL) {    
     // ファイルの読み込みとメモリへの書き込み
-    memset(tmp, '\0', 1024+1);
-    while((readSize = fread(tmp, 1, 1024, fp)) != 0) {
+    memset(tmp, '\0', 2048+1);
+    while((readSize = fread(tmp, 1, 2048, fp)) != 0) {
       strcat(_buf, tmp);
-      memset(tmp, '\0', 1024+1);  // この一行がなく、初期化できていなかったのが原因だった。
+      memset(tmp, '\0', 2048+1);  // この一行がなく、初期化できていなかったのが原因だった。
     }
     printf("\n");
     fclose(fp);
@@ -204,30 +383,133 @@ int test_readFile() {
           欲しい情報が属性、href="ここだったり：）"するのか、ダイレクトに文字列一致の仕組みが必要
           ということになるな。C++ の方がいいような気がしてきたが、まだ遊べるだろ。
     */
-    char pattern[] = "href=";
-    char* start    = &buf[0];
-    char* hitPos   = NULL;
+    char pattern[]  = "href=\"";
+    size_t ptnSize  = strlen(pattern);
+    char* start     = &buf[0];
+    char* hitPos    = NULL;
+    size_t hitCount = 0;
+    H_TREE root     = createTree();
     
     do {
       hitPos = strstr(start, pattern);
       if(hitPos != NULL) {
+        hitPos += ptnSize;
+        hitCount++;
+        pushTree(root, hitPos);
         printf("%p に %s あり\n", hitPos, pattern);
         start = hitPos + 1;
       }
     } while(hitPos != NULL);
+    debug_long("ptnSize is ", &ptnSize);
+    debug_long("hitCount is ", &hitCount);
+    
+    // 実際の値（URL）を取得する
+    H_TREE tmp    = root;
+    char url[1025] = "\0";
+    int i = 0;
+    while((tmp = hasNext(tmp)) != NULL) {
+      char* searchPos = (char*)getValue(tmp);
+      memset(url, '\0', 1025);
+      i = 0;
+      while(1) {
+        if(*searchPos != '"') {
+          url[i] = *searchPos;
+        }
+        if(i == 1024 || *searchPos == '"') {
+          printf("%s\n", url);
+          break;
+        }
+        searchPos++;
+        i++;
+      }
+    }
+    printf("\n");
+    
+    
     /**
           当然だよね、現代の Web とは、JSON と JavaScript で構築されている。
           ベタ書きされた静的な HTML などほぼ無いのではなかろうか。つまり、
           ページを復元するにはそれらもトレースする必要がある orz 大昔にも似
           たような問題に当たったよね：）
+          
+          まぁひとまず、できることはやってみたが、取得した URL が私が欲しか
+          った情報かと問われれば、否である。今回的にしたサイトの場合は、JSON
+          に有益、欲しかった情報があると思われる。
+          
+          上記の仕組みを関数化して、少しだけ汎用性を持たせて、色々調べてみる。
     */
     
     
-    
+    clearTree(root, countTree(root));
     removeBuffer(buf);      
   }
   return EXIT_SUCCESS;  
 }
+
+void searchProto(char* _buf, const char* _pattern, const char _limitCh/*, H_TREE _dest*/) {
+  puts("------ searchProto");
+  if(_buf != NULL) {
+    size_t ptnSize  = strlen(_pattern);
+    char* start     = &_buf[0];
+    char* hitPos    = NULL;
+    H_TREE root     = createTree();
+    size_t hitCount = 0;
+    // 検索文字（パターン）に該当する箇所（アドレス）の取得    
+    do {
+      hitPos = strstr(start, _pattern);
+      if(hitPos != NULL) {
+        hitPos += ptnSize;
+        pushTree(root, hitPos);
+//        printf("%p に %s あり\n", hitPos, _pattern);
+        start = hitPos + 1;
+        hitCount++;
+      }
+    } while(hitPos != NULL);
+    debug_long("hitCount is ", &hitCount);
+    // 実際の値を _limitCh 以前で取得する
+    H_TREE tmp     = root;
+    char url[1025] = "\0";                // 次のステップは、これを返却する仕組みが必要（H_TREE の value その動的メモリでいけるかな、解放忘れ怖いな：）。
+    int i = 0;
+    while((tmp = hasNext(tmp)) != NULL) {
+      char* searchPos = (char*)getValue(tmp);
+      memset(url, '\0', 1025);
+      i = 0;
+      while(1) {
+        if(*searchPos != _limitCh) {
+          url[i] = *searchPos;
+        }
+        if(i == 1024 || *searchPos == _limitCh) {
+          printf("%s\n", url);
+          break;
+        }
+        searchPos++;
+        i++;
+      }
+    }
+    printf("\n");    
+    clearTree(root, countTree(root));
+  } else {
+    printf("_buf is null.\n");
+  }
+}
+
+int test_searchProto() {
+  puts("=== test_searchProto");
+  size_t size = getFileSize(FILE_PATH);
+  char* buf = (char*)malloc(size+1);
+  memset(buf, '\0', size+1);
+  readFile(FILE_PATH, buf);
+  
+  char pattern[]    = "href=\"";
+  searchProto(buf, pattern, '"');
+  char pattern_2[]  = "videoId\":\"";
+  searchProto(buf, pattern_2, '"');
+  
+  removeBuffer(buf);
+  return EXIT_SUCCESS;
+}
+
+
 
 int main(void) {
   puts("START 文字列の解析 ===");
@@ -238,9 +520,14 @@ int main(void) {
     printf("Play and Result ... %d\n", ret = test_Create_Remove_Buf());
     assert(ret == 0);
   }
-  if(1.01) {
+  if(0) {    // 1.01
     int ret = 0;
     printf("Play and Result ... %d\n", ret = test_readFile());
+    assert(ret == 0);
+  }
+  if(1.02) {
+    int ret = 0;
+    printf("Play and Result ... %d\n", ret = test_searchProto());
     assert(ret == 0);
   }
   puts("===   文字列の解析  END");
