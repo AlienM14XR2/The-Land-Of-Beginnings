5acebedea7c39fffb66a98cf526cf626f09e5b60
Jack <alienm14xr2@gmail.com>
2024-03-31 23:12:16 +0900

    C 課題 - Try Catch。 - 個人的な興味。 - H_TREE を別ソースに切り出す、その ２。

---
+++ src/h_tree_proto.c
@@ -0,0 +1,235 @@
+/**
+  課題
+ 
+ H_TREE のブラッシュアップとその動作確認用ソース。
+ 最終的には H_TREE を宣言と定義に切り出すこと。
+ 
+ e.g. compile
+ gcc -O3 -DDEBUG -std=c17 -pedantic-errors -Wall -Werror h_tree_proto.c -o ../bin/main
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <setjmp.h>
+
+
+
+/**
+  Linked List 
+  
+  void* を利用したものを考えてみる。
+    データ構造は  value と next で表現できるはず。
+  push() で追加、pop() で削除する。
+*/
+
+struct tree {
+  void* value;
+  struct tree* next;
+};
+
+typedef void* H_TREE;
+
+/**
+    ツリーハンドラの作成。
+*/
+H_TREE createTree() {
+  puts("--------- createTree");
+  struct tree* pt = (struct tree*)malloc(sizeof(struct tree));
+  pt->value = NULL;
+  pt->next = NULL;
+  return (H_TREE)pt;
+}
+
+/**
+    ツリーハンドラの次の値を返却する。
+*/
+H_TREE hasNext(H_TREE _tree) {
+  return (H_TREE)((struct tree*)_tree)->next;
+}
+
+/**
+    ツリーハンドラのポインタを最後尾に移動する。
+*/
+H_TREE moveLast(H_TREE _tree) {
+  puts("--------- moveLast");
+  struct tree* last = (struct tree*)_tree;
+  while(last != NULL) {
+    if(last->next != NULL) {
+      last = last->next;
+    } else {
+      break;
+    }
+  }
+//  printf("last addr is \t%p\n", (void*)last);
+  return (H_TREE)last;
+}
+
+/**
+    ツリーハンドラの要素数と根の合計数を返却する。
+    根（root）は要素を持たないツリーだが、countTree() は根も
+    含めてそのサイズを返却する。つまり、その値は根 + 要素数と
+    なる。
+*/
+size_t countTree(H_TREE _root) {
+  puts("--------- countTree");
+  struct tree* current = (struct tree*)_root;
+  size_t i = 0;
+  while((current = hasNext(current)) != NULL) {
+    i++;
+  }
+  i += 1;     // これは root も含めて返却している。
+  return i;
+}
+
+/**
+    ツリーハンドラの全要素の削除を行う。
+    根も削除する。
+    ※ もし、H_TREE が管理する value が動的にメモリ確保されたもので、H_TREE 以外そのアドレスを知り得ない場合
+    この関数を value 解放前に行うとメモリリークを引き起こす：）
+*/
+void clearTree(H_TREE _root, size_t count) {  // このことは「肝に銘じよ」C 言語では削除する対象のサイズを先に計算しろ。
+  puts("--------- clearTree");
+  H_TREE array[count];    // この配列の宣言のやり方は GCC 以外でもできるのかな？ 添字に変数を利用しているんだよね。
+  struct tree* current = (struct tree*)_root;
+  array[0] = current;
+  size_t i = 1;
+  while((current = hasNext(current)) != NULL) {
+    array[i] = current;
+    i++;
+  }
+  for(i=0; i<count; i++ ) {
+//    puts("------------ D");
+    free(array[i]);
+  }
+}
+
+/**
+    ツリーハンドラに値を追加する。
+*/
+H_TREE pushTree(H_TREE _root, void* value) {
+  puts("--------- pushTree");
+  struct tree* pt = (struct tree*)malloc(sizeof(struct tree));
+  pt->value = value;
+  pt->next = NULL;
+  struct tree* last = (struct tree*)moveLast(_root);
+  if(last == (struct tree*)_root) {
+//      printf("------------ SAME\n");    
+    ((struct tree*)_root)->next = pt;
+  } else {
+    last->next = pt;
+  }
+//  printf("pt addr is \t%p\n", (void*)pt);
+  return (H_TREE)pt;
+}
+
+void* getValue(H_TREE current) {
+  puts("--------- getValue");
+  if(current != NULL) {
+    return ((struct tree*)current)->value;
+  } else {
+    return NULL;
+  }
+}
+
+/**
+    最後尾にある値の取り出しとそのツリーハンドラの削除を行う。
+  Stack と同じ動作、最初にツリーハンドラに登録した値は、最後に取り出される（First-in Last-out）。
+*/
+void* popStack(H_TREE _root) {
+  puts("--------- popStack");
+  // 前方アドレスの付け替え後、削除する。
+  void* value = NULL;
+  struct tree* last     = (struct tree*)moveLast(_root);
+  struct tree* current  = (struct tree*)_root;
+  if(current == last) {
+    // ignore
+    return NULL;
+  } 
+  else
+  if(current->next == last) {
+//    printf("------------ F2 SAME\n");
+    value = last->value;
+    current->next = NULL;
+    free((void*)last);
+  } 
+  else {
+    while((current = (H_TREE)hasNext((H_TREE)current)) != NULL) {
+      if( ((struct tree*)current)->next == last ) {
+//        printf("------------ SAME\n");
+        value = last->value;
+        ((struct tree*)current)->next = NULL;
+        free((void*)last);
+        break;
+      }
+    }
+  }
+  return value;
+}
+
+/**
+    起点の次、仮の先頭の値の取り出しとそのツリーハンドラの削除を行う。
+  queue と同じ動作（First-in First-out）。
+*/
+void* popQueue(H_TREE _root) {
+  puts("--------- popQueue");
+  // 先頭の ツリーハンドラは起点なので無視する
+  // 2 番目が対象であり、3 番目がある場合はそのアドレスを起点の next に代入する
+  void* value = NULL;
+  struct tree* second = ((struct tree*)_root)->next;
+  if(second != NULL) {
+    value                       = second->value;
+    ((struct tree*)_root)->next = second->next;
+    free((void*)second);
+  }
+  return value;
+}
+
+
+
+//
+// H_TREE 以外の宣言、定義はここから
+//
+
+
+//static jmp_buf jb;
+
+void debug_int(const char* _message, const int* _debug) {
+  printf("DEBUG: %s\tval: %d\taddr: %p\n", _message, *_debug, (void*)_debug);  
+}
+
+void debug_double(const char* _message, const double* _debug) {
+  printf("DEBUG: %s\tval: %lf\taddr: %p\n", _message, *_debug, (void*)_debug);  
+}
+
+void debug_long(const char* _message, const void* _debug) {
+  // -Werror=array-bounds これになる可能性がある、warning だけど、コンパイルオプションで エラーにしている：）
+  // 例えば、実引数に int 型の変数を指定した場合。
+  size_t* psz = (size_t*)_debug;
+  printf("DEBUG: %s\tval: %lu\taddr: %p\n", _message, *psz, _debug);
+}
+
+
+int test_debug_prints() {
+  puts("=== test_debug_prints");
+  int    i    = 3;
+  size_t size = 6;
+  double pi   = 3.141592;
+  debug_int("i \tis "    , &i);
+  debug_long("size \tis ", &size);
+  debug_double("pi \tis ", &pi);
+  return EXIT_SUCCESS;
+}
+
+
+
+int main(void) {
+  puts("START H_TREE のブラッシュアップとその動作確認 ===");
+  if(0.01) {
+    int ret = 0;
+    printf("Play and Result ... \t%d\n", ret = test_debug_prints());
+    assert(ret == 0);
+  }  
+  puts("===   H_TREE のブラッシュアップとその動作確認  END");
+  return 0;
+}
+
