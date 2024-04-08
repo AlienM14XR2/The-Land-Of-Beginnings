d9be76c08cc4c7883e49d1e10f7c5fe13d0650cf
Jack <alienm14xr2@gmail.com>
2024-04-08 14:18:59 +0900

    C 課題 - String の Parse。 - 個人的な興味。 - 関数宣言に簡易コメントを追記した。

--- src/string_parse.c
+++ src/string_parse.c
@@ -76,16 +76,55 @@ static const char* FILE_PATH = "/home/jack/tmp/sample.html";
 */
 
 
-
+/**
+  指定したファイルサイズを取得する。
+*/
 size_t getFileSize(const char* fileName);
+
+/**
+  指定したメモリの開放を行う。
+*/
 void removeBuffer(char* _buffer);
+
+/**
+  ファイル情報の読み込みを行う。
+*/
 void readFile(const char* _filePath, char* _buf);
+
+/**
+  指定したパターンと区切り文字の間の文字列を取得する。
+*/
 void searchProto(H_TREE _dest, size_t _destSize, char* _buf, const char* _pattern, const char _limitCh);
+
+
+/**
+  指定したパターンの範囲を取得する。
+*/
 void setRange(char* _buf, H_TREE _startPositions, H_TREE _endPositions, const char* _startPattern, const char* _endPattern);
+
+/**
+  範囲の整合性を確かめる。
+*/
 bool isValidRange(H_TREE _startPos, H_TREE _endPos);
+
+/**
+  開始文字と終了文字のカウントを取得する。
+*/
 void checkChar(const char* _src, const char _start, const char _end, size_t* _startCount, size_t* _endCount);
+
+/**
+  文字の先頭に任意の文字を指定回数分追加する。
+*/
 void appendTop(H_TREE _dest, const char* _src, const char* _append, const size_t _chCount);
+
+/**
+  文字の終端に任意の文字を指定回数分追加する。
+*/
 void appendBottom(H_TREE _dest, const char* _src, const char* _append, const size_t _chCount);
+
+/**
+  指定した範囲の文字列を取得する。
+*/
 void search2nd(H_TREE _dest, H_TREE _startPos, H_TREE _endPos, char _beforeLimitPos);
 
 
